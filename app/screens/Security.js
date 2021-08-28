94843223b420617f4d5f30cf1e0fba4fd0b050d3
faisal932 <faisal932@gmail.com>
2021-08-27 19:49:50 -0700

    Status bar updating

--- app/screens/Security.js
+++ app/screens/Security.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,StatusBar } from 'react-native'
 import { Appbar, PrivacyLastBox, SecurityFirstBox } from '../components'
 
 export const Security = () => {
     return (
         <View>
-            
+            <StatusBar backgroundColor = "#437540" />
             <Appbar iconname = "arrowleft" name = "Security" />
             <SecurityFirstBox />
             <PrivacyLastBox header = "Show security notifications" description = {`Turn on this setting to receive notifications
